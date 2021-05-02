68245e5817e6a9cd287a0e59b11d9bda1a182ae0
SreshthaSaha <65612941+SreshthaSaha@users.noreply.github.com>
2021-05-02 09:29:04 +0530

    Add files via upload

---
+++ screens/CreatePasswordScreen.js
@@ -0,0 +1,10 @@
+import React,{Component}from 'react';
+import {View,Text,TextInput,Modal,StyleSheet,TouchableOpacity,Alert,ScrollView,KeyboardAvoidingView} from 'react-native';
+
+export default class CreatePasswordScreen extends Component{
+  render(){
+      return(
+          <Text>This is the Create Password Screen</Text>
+      );
+  }
+}
